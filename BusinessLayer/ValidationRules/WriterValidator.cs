1b0345db12ccfb6001989c39132a5a8f09d41d4c
Oktay EFE <oktayefe56@gmail.com>
2022-09-01 22:35:16 +0300

    Writer user add

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class WriterValidator : AbstractValidator<Writer>
+	{
+		public WriterValidator()
+		{
+			RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail Adresi Boş Geçilemez");
+			RuleFor(x => x.WriterName).NotEmpty().WithMessage("Ad Soyad Kısmı Boş Geçilemez");
+			RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Parola kısmı Boş Geçilemez");
+			RuleFor(x => x.WriterImage).NotEmpty().WithMessage("Görsel Kısmı Boş Geçilemez");
+			RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen 2 karakterden fazla giriş yapınız");
+			RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen 50 karakterden az giriş yapınız");
+		}
+	}
+}
+
--- MSY/Controllers/RegisterController.cs
+++ MSY/Controllers/RegisterController.cs
@@ -1,25 +1,40 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MSY.Controllers
 {
-    public class RegisterController : Controller
-    {
-        WriterManager writerManager = new WriterManager(new EfWriterRepository());
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Index(Writer _writer)
-        {
-            _writer.WriterAbout = "-";
-            _writer.WriterStatus = true;
-            writerManager.WriterAdd(_writer);
-            return RedirectToAction("Index","Blog");
-        }
-    }
+	public class RegisterController : Controller
+	{
+		WriterManager writerManager = new WriterManager(new EfWriterRepository());
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer _writer)
+		{
+			WriterValidator wv = new WriterValidator();
+			ValidationResult result = wv.Validate(_writer);
+			if (result.IsValid)
+			{
+				_writer.WriterAbout = "-";
+				_writer.WriterStatus = true;
+				writerManager.WriterAdd(_writer);
+				return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach (var item in result.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+	}
 }
