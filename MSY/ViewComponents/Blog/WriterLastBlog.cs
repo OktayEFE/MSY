39f2815a2bb2bcd74ab156a22c9550e629993b7e
Oktay EFE <oktayefe56@gmail.com>
2022-09-03 12:10:36 +0300

    Writer add blog writer

---
+++ MSY/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSY.ViewComponents.Blog
+{
+	public class WriterLastBlog:ViewComponent
+	{
+		BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = _blogManager.GetBlogListByWriter(1);
+			return View(values);
+		}
+	}
+}
+
